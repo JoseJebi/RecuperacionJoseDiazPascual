95ce9314514f9fbdcb518436d1f03d0a6cbb03d6
JOSEPDAM\infci <jprodrigogarcia01.26@gmail.com>
2025-03-25 08:54:59 +0100

    Agregar archivos de proyecto.

---
+++ RecuperacionJoseDiazPascual/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RecuperacionJoseDiazPascual
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RecuperacionJoseDiazPascual/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RecuperacionJoseDiazPascual
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
