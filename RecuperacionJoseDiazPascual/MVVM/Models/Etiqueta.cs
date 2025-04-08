bedbbadeea8afb3f773da2a710fc38b47e6f08f7
DiazJose-w <jdiazlava@gmail.com>
2025-04-08 10:19:01 +0200

    Clase etiqueta terminada

--- RecuperacionJoseDiazPascual/MVVM/Models/Etiqueta.cs
+++ RecuperacionJoseDiazPascual/MVVM/Models/Etiqueta.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RecuperacionJoseDiazPascual.Abstractions;
+using SQLite;
 
 namespace RecuperacionJoseDiazPascual.MVVM.Models
 {
-    internal class Etiqueta
+    [Table("Etiqueta")]
+    public class Etiqueta : TableData
     {
+        [Column("titulo"), Indexed, NotNull]
+        public string Titulo { get; set; }
     }
 }
